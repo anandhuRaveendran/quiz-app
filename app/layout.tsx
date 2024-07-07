77f65d9108a66856ac69926187e2eac3f57cc61f
anandhuRaveendran <anandhuraveendrank@gmail.com>
2024-07-08 02:49:27 +0530

    result page updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className="flex min-h-screen sm:p-10 justify-center items-center bg-[#c8c7ff]">
 
         <main>{children}</main>
         
--- components/Quiz.tsx
+++ components/Quiz.tsx
@@ -23,7 +23,9 @@ const questions: Question[] = [
   { no: '1', question: "What is the capital of France?", options: ["Berlin", "Madrid", "Paris", "Rome"], answer: "Paris" },
   { no: '2', question: "Which planet is known as the Red Planet?", options: ["Earth", "Mars", "Jupiter", "Saturn"], answer: "Mars" },
   { no: '3', question: "Which planet is known as the Red Planet?", options: ["Earth", "Mars", "Jupiter", "Saturn"], answer: "Mars" },
-  { no: '4', question: "Which planet is known as the Red Planet?", options: ["Earth", "Mars", "Jupiter", "Saturn"], answer: "Mars" }
+  { no: '4', question: "Which planet is known as the Red Planet?", options: ["Earth", "Mars", "Jupiter", "Saturn"], answer: "Mars" },
+ 
+
 ];
 
 interface Answer {
@@ -117,52 +119,67 @@ const Quiz: React.FC = () => {
 
   if (showSummary) {
     return (
-      <div className="flex flex-col align-middle m-32">
-        <div className='flex flex-col space-y-4 ml-auto mr-auto border'>
-          {summary == true ? <div className='flex flex-col align-middle  p-2 '><h2 className="text-2xl font-bold">Quiz Summary</h2>
-            {answers.map((answer, index) => (
-              <div key={index} className="mb-2">
-                <p>{answer.question}</p>
-                <p>Your answer: {answer.selectedOption}</p>
-                <p>Correct answer: {answer.correctAnswer}</p>
-              </div>
-            ))}</div> : <div></div>}
-        </div>
-        <p>Your final score: {score}</p><div className='flex flex-col align-middle'>
 
-        <button className="mt-1 ml-auto mr-auto p-2 bg-blue-500 text-white border rounded-lg cursor-pointer  align-middle h-10" onClick={() => setSummary(true)}>Quiz Summary</button>
-        <div className='flex flex-col space-y-4 ml-auto mr-auto w-40'>
-          <button className="mt-1 ml-auto mr-auto p-2 bg-blue-500 text-white border rounded-lg cursor-pointer  align-middle h-10" onClick={handleRestart}>Restart Quiz</button>
-          <button className="mt-1 ml-auto mr-auto p-2 bg-blue-500 text-white border rounded-lg cursor-pointer  align-middle h-10" onClick={handleShare} >Share</button>
-        </div>
-        <div className='share-design'>
-          {share == true ? <div>
-            <FacebookShareButton url={'http://localhost:3000'}>
-              <FacebookIcon size={32} round />
-            </FacebookShareButton>
-            <RedditShareButton url={'http://localhost:3000'}>
-              <RedditIcon size={32} round />
-            </RedditShareButton>
-            <WhatsappShareButton url={'http://localhost:3000'}>
-              <WhatsappIcon size={32} round />
-            </WhatsappShareButton>
-            <LinkedinShareButton url={'http://localhost:3000'}>
-              <LinkedinIcon size={32} round />
-            </LinkedinShareButton>
-          </div> : ''}
-        </div>
-        </div>
+      <div className="flex min-h-screen sm:p-10 justify-center items-center bg-[#c8c7ff]">
 
+      <div className="flex flex-col sm:flex-row sm:w-[60rem] sm:h-[40rem] sm:rounded-3xl h-full w-full sm:shadow-lg bg-white">
+      <div className="flex sm:basis-1/2 flex-col items-center justify-center bg-gradient-to-b to-[#2e2be9] from-[#7857ff] sm:rounded-3xl rounded-b-3xl py-7 px-10">
+          <h2 className="text-[#ebf1ff] font-hankengrotesk text-xl sm:text-lg font-bold">Your Result</h2>
+          <div className="flex flex-col items-center justify-center w-44 h-44 rounded-full bg-gradient-to-b from-[#2421ca]/100 to-[#4e21ca]/0 my-6">
+              <h1 className="text-white font-hankengrotesk text-7xl sm:text-6xl font-extrabold">{score*10}</h1>
+              <p className="text-[#c8c7ff] text-lg sm:text-base font-hankengrotesk">of {answers.length*10}</p>
+          </div>
+          <h1 className="text-white font-hankengrotesk text-3xl sm:text-2xl mb-2 font-extrabold">Great</h1>
+          <p className="text-center text-[#ebf1ff] text-lg sm:text-base font-hankengrotesk">You scored higher than 65% of the people who have taken these tests.</p>
       </div>
+      <div className="flex sm:basis-1/2 flex-col min-h-auto justify-between space-y-5 p-8 sm:px-10">
+          <h1 className="font-hankengrotesk text-xl sm:text-lg font-bold text-red-500">Summary</h1>
+          <div className="flex flex-col space-y-4 overflow-auto">
+            {<div>
+           {answers.map((answer, index) => (
+              <div key={index} className="mb-2">
+                <p className='text-blue-500'>{answer.question}</p>
+                {answer.selectedOption==answer.correctAnswer?<p className="text-green-500">Your answer: {answer.selectedOption}</p>:<p className="text-red-500">Your answer: {answer.selectedOption}</p>}
+                <p>Correct answer: <span className='text-yellow-500'>{answer.correctAnswer}</span></p>
+              </div>
+            ))}</div>}
+          
+          </div>
+          <button className="bg-[#303b5a] sm:hover:bg-gradient-to-b to-[#2e2be9] from-[#7857ff]
+           active:bg-gradient-to-b text-white py-4 sm:py-3 rounded-full font-hankengrotesk text-xl 
+           sm:text-lg font-bold" onClick={handleRestart}>Restart</button>
+          <button className="bg-[#303b5a] sm:hover:bg-gradient-to-b to-[#2e2be9] from-[#7857ff] 
+          active:bg-gradient-to-b text-white py-4 sm:py-3 rounded-full font-hankengrotesk
+           text-xl sm:text-lg font-bold" onClick={handleShare}>Share</button>
+ <div className='py-4 sm:py-3 '>
+           {share == true ? <div>
+             <FacebookShareButton url={'http://localhost:3000'}>
+               <FacebookIcon size={32} round />
+             </FacebookShareButton>
+             <RedditShareButton url={'http://localhost:3000'}>
+               <RedditIcon size={32} round />
+             </RedditShareButton>
+             <WhatsappShareButton url={'http://localhost:3000'}>
+               <WhatsappIcon size={32} round />
+             </WhatsappShareButton>
+             <LinkedinShareButton url={'http://localhost:3000'}>
+               <LinkedinIcon size={32} round />
+             </LinkedinShareButton>
+           </div> : ''}
+         </div>
+      </div>
+      </div>
+  </div>
+
     );
   }
 
-  return (<div className='m-32 border shadow-2xl rounded-lg'>
+  return (<div className='mx-96 my-32 border-2 shadow-2xl rounded-lg bg-white'>
 
   
     <div className="flex flex-col align-middle justify-items-center space-y-2 m-6">
 
-      <div className=" flex flex-col align-middle justify-center w-full">
+      <div className=" flex flex-col w-full">
       <h2 className="text-3xl font-bold text-red-600">{questions[currentQuestionIndex].no}:{questions[currentQuestionIndex].question}</h2>
 
       </div>
